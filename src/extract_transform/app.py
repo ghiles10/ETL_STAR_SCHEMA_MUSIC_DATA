03e5c3db9c61636e780feda50eb7dc18f25bf2a4
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-01-16 23:24:43 +0000

    fix logging

--- src/extract_transform/app.py
+++ src/extract_transform/app.py
@@ -6,8 +6,8 @@ from extract_all import main_extract_data
 from sql_query.create_table import main_create_table 
 
 
-if not os.path.exists("./log"):
-    os.mkdir("log")
+if not os.path.exists("src/log"):
+    os.mkdir("src/log")
 
 if __name__ == "__main__":
     main_create_table()
